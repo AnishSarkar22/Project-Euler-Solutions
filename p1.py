d6fa48792de20c115537d1ea68608f30863b10e7
Anish Sarkar <104695310+AnishSarkar22@users.noreply.github.com>
2025-07-20 19:06:26 +0530

    first commit

---
+++ p1.py
@@ -0,0 +1,11 @@
+
+
+def sum():
+    total = 0
+    for i in range(1000):
+        if i % 3 == 0 or i % 5 == 0:
+            total += i
+    return total
+    
+if __name__ == "__main__":
+    print(sum())
